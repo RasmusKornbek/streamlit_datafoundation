cfdb957372e7fc29b7c44c9772badde29fc51b4e
RasmusKornbek <35880535+RasmusKornbek@users.noreply.github.com>
2023-09-26 14:09:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,7 +105,6 @@ styled_df = df_selection.style.applymap(lambda x: get_color(x), subset=['Analyst
 ##st.write(styled_df, unsafe_allow_html=True, use_container_width=True)
 
 st.subheader('Who is your stakeholder?')
-showData=st.multiselect('Filter: ',styled_df.columns,default=["Analyst","IT","Business"])
 
 option = st.selectbox(
     'Who is your stakeholder?',
