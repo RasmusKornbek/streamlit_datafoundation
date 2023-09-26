8d0228506c80a98d6a9a2d4633c6f58e7e6a52e9
RasmusKornbek <35880535+RasmusKornbek@users.noreply.github.com>
2023-09-26 10:49:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,7 +94,7 @@ def get_color(val):
     return f'background-color: rgb(0, {int(255 - normalized_val * 255)}, 0)'
 
 # Apply the color gradient to the respective columns
-##styled_df = df_selection.style.applymap(lambda x: get_color(x), subset=['Analyst', 'IT', 'Business'])
+styled_df = df_selection.style.applymap(lambda x: get_color(x), subset=['Analyst', 'IT', 'Business'])
 
 # Display the styled DataFrame to fill the entire section
 ##st.write(styled_df, unsafe_allow_html=True, use_container_width=True)
