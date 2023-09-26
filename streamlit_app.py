8e07c6c179fcf58ed18fe75d30c6e90066616971
RasmusKornbek <35880535+RasmusKornbek@users.noreply.github.com>
2023-09-26 11:15:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,7 +102,7 @@ styled_df = df_selection.style.applymap(lambda x: get_color(x), subset=['Analyst
 
 def Home():
     with st.expander("My Data", expanded=True):
-        st.dataframe(styled_df, use_container_width=True)
+        st.dataframe(styled_df['Question'], use_container_width=True)
 Home()
 
 #st.write(styled_df, unsafe_allow_html=True, use_container_width=use_container_width)
